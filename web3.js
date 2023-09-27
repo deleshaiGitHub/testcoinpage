63af60d46ebf6a2beb8e4d7b1f26ef0717c83722
Mark <88356506+deleshaiGitHub@users.noreply.github.com>
2023-09-27 08:43:24 -0400

    Update web3.js

--- web3.js
+++ web3.js
@@ -14,13 +14,14 @@ web3.eth.net.isListening()
 
 // Request user permission to connect their Ethereum wallet (optional)
 if (typeof window.ethereum !== 'undefined') {
-    try {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        const userAddress = accounts[0];
-        console.log('Connected wallet address:', userAddress);
-    } catch (error) {
-        console.error('Wallet connection error:', error);
-    }
+    window.ethereum.enable()
+        .then(accounts => {
+            // User has approved access to their wallet
+            const userAddress = accounts[0];
+            console.log('Connected wallet address:', userAddress);
+        })
+        .catch(error => console.error('Wallet connection error:', error));
 }
+
 // Export the Web3 instance for use in other parts of your DApp (optional)
 module.exports = web3;
