1fcdebdcabbc13cd75a22d82ee8c94bc41837788
Mark <88356506+deleshaiGitHub@users.noreply.github.com>
2023-10-01 12:25:17 -0400

    Update and rename abi.js to app.js

---
+++ app.js
@@ -0,0 +1,232 @@
+document.addEventListener("DOMContentLoaded", async function () {
+    if (typeof web3 !== "undefined") {
+        // Use the web3.js provided by MetaMask or other Ethereum provider
+        window.web3 = new Web3(web3.currentProvider);
+    } else {
+        alert("No Ethereum provider detected. You need MetaMask or a similar extension.");
+    }
+
+    // Initialize the Bliss contract instance
+    const blissContractAddress = "YOUR_BLISS_CONTRACT_ADDRESS"; // Replace with your deployed Bliss contract address
+    const blissContractABI = [
+        // Paste the ABI of the Bliss contract here
+    ];
+    const blissContract = new web3.eth.Contract(blissContractABI, blissContractAddress);
+
+    // Get and display the BLISS token balance
+    const userAddress = await web3.eth.getAccounts();
+    const blissBalance = await blissContract.methods.balanceOf(userAddress[0]).call();
+    document.getElementById("blissBalance").textContent = `${web3.utils.fromWei(blissBalance)} BLISS`;
+});
+
+async function swapTokens() {
+    const amount = document.getElementById("amount").value;
+
+    if (!amount || amount <= 0) {
+        alert("Please enter a valid amount.");
+        return;
+    }
+
+    const blissContractAddress = "0xF055ee36f683Ed9d7749a20a06ceE0b4585f75e5"; // Replace with your deployed Bliss contract address
+    const blissContractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "mintTokens",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "initialSupply",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "oldTokensAmount",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "newTokensAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "Swap",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "swapTokens",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+    const blissContract = new web3.eth.Contract(blissContractABI, blissContractAddress);
+
+    const userAddress = await web3.eth.getAccounts();
+    const swapAmount = web3.utils.toWei(amount.toString(), "ether");
+
+    try {
+        await blissContract.methods.swapTokens(swapAmount).send({ from: userAddress[0] });
+        alert("Swap successful!");
+        // Refresh the BLISS token balance
+        const blissBalance = await blissContract.methods.balanceOf(userAddress[0]).call();
+        document.getElementById("blissBalance").textContent = `${web3.utils.fromWei(blissBalance)} BLISS`;
+    } catch (error) {
+        console.error(error);
+        alert("Swap failed. Check your wallet and try again.");
+    }
+}
